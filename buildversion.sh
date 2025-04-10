a97b46025b99f2c16551bb5600a9f28248620092
Frank Menke <frank.menke@cchmc.org>
2025-04-10 09:41:55 -0400

    version var

--- buildversion.sh
+++ buildversion.sh
@@ -1,14 +1,15 @@
 #!/bin/zsh
-dotnet build -p:Version=15.1.4-cchmc-1.7 -c Debug
-dotnet pack -p:Version=15.1.4-cchmc-1.7 -c Debug
-dotnet nuget push --source "CCHMC.Common" --api-key whatev HotChocolate/AspNetCore/src/Transport.Http/bin/Debug/HotChocolate.Transport.Http.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev HotChocolate/AspNetCore/src/Transport.Abstractions/bin/Debug/HotChocolate.Transport.Abstractions.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev HotChocolate/Utilities/src/Utilities/bin/Debug/HotChocolate.Utilities.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev HotChocolate/Language/src/Language.SyntaxTree/bin/Debug/HotChocolate.Language.SyntaxTree.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Core/bin/Debug/StrawberryShake.Core.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Transport.Http/bin/Debug/StrawberryShake.Transport.Http.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Transport.WebSockets/bin/Debug/StrawberryShake.Transport.WebSockets.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Razor/bin/Debug/StrawberryShake.Razor.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/MetaPackages/Blazor/bin/Debug/StrawberryShake.Blazor.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Tooling/src/dotnet-graphql/nupkg/StrawberryShake.Tools.15.1.4-cchmc-1.7.nupkg
-dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Resources/bin/Debug/StrawberryShake.Resources.15.1.4-cchmc-1.7.nupkg
+version="15.1.4-cchmc-1.9"
+dotnet build -p:Version=$version -c Debug
+dotnet pack -p:Version=$version -c Debug
+dotnet nuget push --source "CCHMC.Common" --api-key whatev HotChocolate/AspNetCore/src/Transport.Http/bin/Debug/HotChocolate.Transport.Http.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev HotChocolate/AspNetCore/src/Transport.Abstractions/bin/Debug/HotChocolate.Transport.Abstractions.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev HotChocolate/Utilities/src/Utilities/bin/Debug/HotChocolate.Utilities.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev HotChocolate/Language/src/Language.SyntaxTree/bin/Debug/HotChocolate.Language.SyntaxTree.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Core/bin/Debug/StrawberryShake.Core.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Transport.Http/bin/Debug/StrawberryShake.Transport.Http.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Transport.WebSockets/bin/Debug/StrawberryShake.Transport.WebSockets.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Razor/bin/Debug/StrawberryShake.Razor.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/MetaPackages/Blazor/bin/Debug/StrawberryShake.Blazor.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Tooling/src/dotnet-graphql/nupkg/StrawberryShake.Tools.$version.nupkg
+dotnet nuget push --source "CCHMC.Common" --api-key whatev StrawberryShake/Client/src/Resources/bin/Debug/StrawberryShake.Resources.$version.nupkg
